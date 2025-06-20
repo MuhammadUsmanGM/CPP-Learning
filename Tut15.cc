936bef752bdb2311d8ae4fe42801841d8a757520
Muhammad Usman GM <muhammadusman75425965etc@gamil.com>
2025-06-20 12:32:18 +0500

    Learning progress update

---
+++ Tut15.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+//Recursion & Recursive func
+
+int factorial(int n){
+    if (n<=1){
+        return 1;
+    }
+    return n*factorial(n-1);
+    //recursion is a func calling itself
+}
+
+int fib(int n){
+    if (n<2){return 1;}
+    return fib(n-2) + fib(n-1);
+}
+int main(){
+    int a;
+    cout<<"Enter a number: ";
+    cin>>a;
+    cout<<"The factorial of "<<a<<" is "<<factorial(a)<<endl;
+    cout<<"The term in fibonacci sequence at position "<<a<<" is "<<fib(a)<<endl;
+}
