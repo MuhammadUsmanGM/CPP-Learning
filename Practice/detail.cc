a9d5f79a5bc22949883f55a472cd8fec313333ef
Muhammad Usman GM <muhammadusman75425965etc@gamil.com>
2025-07-17 16:49:30 +0500

    practice update

--- Practice/detail.cc
+++ Practice/detail.cc
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <vector>
 using namespace std;
 
 class Student
@@ -9,6 +10,7 @@ class Student
     protected:
         string name;
         int rollNo;
+        int totalStudent;
     public:
         void set(string n,int r){
             name = n;
@@ -19,33 +21,43 @@ class Marks
 {
     protected:
         int subject;
-        float marks[];
+        vector<float> marks;
 };
 class InputDisplay : public Student , public Marks
 {
     public:
         void input(){
-            cout<<"Enter Student name: ";
-            getline(cin,name);
-            cout<<"Enter roll number : ";
-            cin>>rollNo;
-            cout<<"Enter the number of subjects: ";
-            cin>>subject;
-            for (int i=0;i<subject;i++){
-                cout<<"Enter marks for subject "<<i+1<<" : ";
-                cin>>marks[i];
+            cout<<"Enter the total number of students : ";
+            cin>>totalStudent;
+            for (int i=0;i<totalStudent;i++){
+                cout<<"---Enter INFO for Student "<<i+1<<"---"<<endl;
+                cout<<"Enter Student name: ";
+                cin.ignore();
+                getline(cin,name);
+                cout<<"Enter roll number : ";
+                cin>>rollNo;
+                cout<<"Enter the number of subjects: ";
+                cin>>subject;
+                marks.resize(subject);
+                for (int i=0;i<subject;i++){
+                    cout<<"Enter marks for subject "<<i+1<<" : ";
+                    cin>>marks[i];
+                }
             }
         }
         void display(){
             ofstream out;
-            out.open("detail.txt");
-            out<<"Student Name: "<<name<<endl;
-            out<<"Roll Number: "<<rollNo<<endl;
-            out<<"Number of Subjects: "<<subject<<endl;
-            for (int j=0;j<subject;j++){
-                out<<"Marks for subject "<<j+1<<" : "<<marks[j]<<endl;
+            out.open("detail.txt", ios::app);
+            for (int i=0;i<totalStudent;i++){
+                out<<"---INFO for Student "<<i+1<<"---"<<endl;
+                out<<"Student Name: "<<name<<endl;
+                out<<"Roll Number: "<<rollNo<<endl;
+                out<<"Number of Subjects: "<<subject<<endl;
+                for (int j=0;j<subject;j++){
+                    out<<"Marks for subject "<<j+1<<" : "<<marks[j]<<endl;
+                }
+                out.close();
             }
-            out.close();
         }
 };
 
